00d62b2ec5bb525e272130872c6eb49658da02ff
liuqiao <liu_qiao@chinatelecom.cn>
2020-02-07 16:32:01 +0800

    增加baseUrl

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 
 module.exports = {
+    baseUrl: '/vue-pro-action/',
     devServer: {
         disableHostCheck: true,
         contentBase: path.join(__dirname, '.'),
